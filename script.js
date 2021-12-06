20400100cd9d70eb2051221c3d4ee0adf17b95bc
Ethan Buchanan <ethan@buchananville.com>
2021-12-06 00:44:10 -0800

    added saving entered text feature, along with updating styling to reflect current time

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 var timeBlockContainerEL = $("ul");
 
-function createTimeBlock(time)
+
+
+function createTimeBlock(time,hour)
 {
+    var got = String( localStorage.getItem(time));
+    var now = moment().format("H");
+    console.log(now);
     var output = $(`
-    <li id="${time}" class="row time-block past">
+    <li id="${time}" class="row time-block ${hour < now? "past":(hour > now?"future":"present")}">
           <div class="hour">
             ${time}  
           </div >
-          <textarea class="description"></textarea>
+          <textarea class="description">${got === "null"?"":got }</textarea>
           <button class="saveBtn">💾</button>
         </li>
     `);
+
+    output.children("button").on("click",function ()
+    {
+        //console.log(output.children("textarea").val());
+        localStorage.setItem(time,output.children("textarea").val());
+    })
     return output; 
 }
-console.log(timeBlockContainerEL);
-for (var i = 9; i < 13; i++){
-    timeBlockContainerEL.append(createTimeBlock(i+":AM"));
+
+
+for (var i = 9; i <= 12; i++){
+    timeBlockContainerEL.append(createTimeBlock(i+":AM",i));
 }
-for (var i = 1; i < 6; i++){
-    timeBlockContainerEL.append(createTimeBlock(i+":PM"));
+for (var i = 1; i <= 5; i++){
+    timeBlockContainerEL.append(createTimeBlock(i+":PM",i+12));
 }
+
+var now = moment().format("dddd, MMMM Do");
+$("#currentDay").text(now);
